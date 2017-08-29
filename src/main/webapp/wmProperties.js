fb85d4853d94ea451d8220b67ac682f6b6625786
QmLbNkUbWQ <automate1@wavemaker.com>
2017-08-29 09:34:57 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_gnegraVYPe",
+  "homePage" : "Main",
+  "name" : "Auto_gnegraVYPe",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
